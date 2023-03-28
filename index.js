aaa857aa63f420fab973663ba073e3cd702b14e6
Ishan Rohatgi <ishan.77775@gmail.com>
2023-03-28 10:48:20 +0530

    video Issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const url = req.query.url;
 const title =req.query.title;
 
  ytdl(url, { quality: 'highest' }).pipe(res);
- res.header('Access-Control-Allow-Origin', 'https://youtube-saver.netlify.app/');
+ res.header('Access-Control-Allow-Origin', 'https://youtube-saver.netlify.app');
  res.setHeader('Content-Type', 'video/mp4');
  res.header('Content-Disposition', `attachment; filename=${title}.mp4`);
 })
