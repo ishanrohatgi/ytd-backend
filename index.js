2ed170f963ec69f2f92939aa992d63d31051e877
Ishan Rohatgi <ishan.77775@gmail.com>
2023-03-28 10:37:23 +0530

    video Issue

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const url = req.query.url;
 const title =req.query.title;
 
  ytdl(url, { quality: 'highest' }).pipe(res);
+ res.header('Access-Control-Allow-Origin', '*');
  res.setHeader('Content-Type', 'video/mp4');
  res.header('Content-Disposition', `attachment; filename=${title}.mp4`);
 })
