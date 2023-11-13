b12548101da3d4b742ff906dfef74f64f4b81c0c
ishanrohatgi <66031300+ishanrohatgi@users.noreply.github.com>
2023-11-14 03:56:14 +0530

    reverted .

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 import express, { response } from 'express';
 import ytdl from 'ytdl-core';
-const app = express();
 import cors from 'cors';
+const app = express();
 app.use(cors());
 
-app.get('/',async (req, res)=>{
-const videoURL = req.query.url; // Assuming the URL is passed as a query parameter
- 
-  const outputPath = `{req.query.title}.mp4`; // Output file name
+app.get('/',  cors() ,async (req, res)=>{
+const url = req.query.url;
+const title =req.query.title;
+const videoInfo = await ytdl.getInfo(url);
+const format = videoInfo.formats.reduce((prev, current) => {
+    if (prev.resolution == null) return current;
+    if (current.resolution == null) return prev;
+    return parseInt(current.resolution.slice(0, -1)) > parseInt(prev.resolution.slice(0, -1)) ? current : prev;
+  });
+res.status(206);
+// Get start and end bytes from request headers
+  const range = req.headers.range;
+  const fileSize = parseInt(videoInfo.videoDetails.lengthSeconds) * 1000000;
+  const CHUNK_SIZE = 10 ** 6; // 1MB
+  const start = Number(range?.replace(/\D/g, ''));
+  const end = Math.min(start + CHUNK_SIZE, fileSize - 1);
 
-  try {
-    const videoInfo = await ytdl.getInfo(videoURL);
-    const videoFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highest' });
-
-    if (videoFormat) {
-      res.header('Content-Disposition', `attachment; filename="${videoInfo.title}.mp4"`);
-      ytdl(videoURL)
-        .pipe(res);
-    } else {
-      res.status(404).send('No suitable video format found.');
-    }
-  } catch (error) {
-    res.status(500).send('Error downloading the video.');
-  }
+  // Set response headers for partial content
+  res.setHeader('Content-Length', end - start + 1);
+  res.setHeader('Content-Range', `bytes ${start}-${end}/${fileSize}`);
+ ytdl(url, { format }).pipe(res, { start, end });
+ res.setHeader('Content-Type', 'video/mp4');
+ res.header('Content-Disposition', `attachment; filename=${title}.mp4`);
 })
 
 
